243faf920d20b3bb1eb8c6358100c974fa8bf1a5
Martin Angulo <martin.angulo@devnavigate.com>
2025-02-21 00:30:44 -0500

    fix index order

--- src/controllers/student-attendance/index.ts
+++ src/controllers/student-attendance/index.ts
@@ -1,8 +1,8 @@
 'use strict'
 
+export { getClassReportByClassIdForAdmin } from './get-class-report-by-class-id-for-admin-controller'
 export { getDailyReportForAdmin } from './get-daily-report-for-admin-controller'
 export { getStudentAttendancesByDay } from './get-student-attendances-by-day-controller'
+export { getStudentReportForAdmin } from './get-student-report-for-admin-controller'
 export { registerStudentAttendance } from './register-student-attendance-controller'
 export { updateStudentAttendanceById } from './update-student-attendance-by-id-controller'
-export { getClassReportByClassIdForAdmin } from './get-class-report-by-class-id-for-admin-controller'
-export { getStudentReportForAdmin } from './get-student-report-for-admin-controller'
