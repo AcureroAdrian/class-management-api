da5ea9cfec6b233bffe8597332e9c3a33d371eff
Adrian Acurero <adrianacurero@gmail.com>
2025-07-28 11:37:04 -0500

    Refactorizar el controlador de asistencia estudiantil para mejorar la lógica de generación de asistencias virtuales y ajustar la obtención del nombre del día de la semana según la zona horaria de Houston.

--- src/utils/get-name-of-week-day-by-day.ts
+++ src/utils/get-name-of-week-day-by-day.ts
@@ -3,7 +3,7 @@ import { TDaysOfWeek } from './common-types'
 type TGetNameOfWeekDayByDay = (day: Date) => TDaysOfWeek
 
 const getNameOfWeekDayByDay: TGetNameOfWeekDayByDay = (day: Date) => {
-	const weekDay = day.toLocaleString('en-US', { weekday: 'long' })
+	const weekDay = day.toLocaleString('en-US', { weekday: 'long', timeZone: 'America/Chicago' })
 	return weekDay.toLowerCase() as TDaysOfWeek
 }
 
